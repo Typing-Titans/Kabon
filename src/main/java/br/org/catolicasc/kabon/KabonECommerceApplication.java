cc080e4fda81e7020987b7bd80bf89f1d3d6fe72
sirotaiv <otaviorau@gmail.com>
2024-03-06 21:10:44 -0300

    First Commit

---
+++ src/main/java/br/org/catolicasc/kabon/KabonECommerceApplication.java
@@ -0,0 +1,13 @@
+package br.org.catolicasc.kabon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KabonECommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KabonECommerceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/org/catolicasc/kabon/KabonECommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.catolicasc.kabon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KabonECommerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
